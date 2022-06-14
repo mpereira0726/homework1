5c383eeed1fb4ba65b9270af42ae0d089c40b64c
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-06-14 16:14:29 -0400

    updated homework1.java

---
+++ src/homework1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class homework1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of months: ");
+        int months = input.nextInt();
+
+        int weeks = months*4;
+        int days = weeks*7;
+        int hours = days*24;
+        int minutes = hours*60;
+        int seconds = minutes*60;
+
+        System.out.println("Weeks: "+weeks);
+        System.out.println("Days: "+days);
+        System.out.println("Hours: "+hours);
+        System.out.println("Minutes: "+minutes);
+        System.out.println("Seconds: "+seconds);
+
+
+
+    }
+}
